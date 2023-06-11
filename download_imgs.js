fe059d76bc7e03e9a100a054593bb70c150a51de
SgO <santiagogar2053@gmail.com>
2023-06-11 02:29:36 -0300

    removed comments so it works

--- download_imgs.js
+++ download_imgs.js
@@ -1,16 +1,9 @@
-// Select all <img> elements with style="width: 100%;" and src attribute starting with "blob"
-const images = document.querySelectorAll('img[style="width: 100%;"][src^="blob"]');
+const images = document.querySelectorAll('img:not([draggable="false"])[style="width: 100%;"][src^="blob"]');
 
-// Iterate over the selected images
 images.forEach((image, index) => {
-  // Extract the image URL
   const imageURL = image.src;
-
-  // Create a link element
   const link = document.createElement('a');
   link.href = imageURL;
-  link.download = `automatic_whatsap_image_scraper_${index + 1}.jpg`; // Set a unique name for each downloaded image
-
-  // Simulate a click event on the link to trigger the download
+  link.download = `automatic_whatsap_image_scraper_${index + 1}.jpg`;
   link.click();
 });
